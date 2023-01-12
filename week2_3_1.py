8d8350f7abe1b13af5f6710f160904a5487a1bf2
SweepingBishops <roshan.thakidiyel@gmail.com>
2023-01-12 19:25:34 +0530

    Commits

---
+++ week2_3_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+input_list = [31,12,21,55,14,1,51,30,2,7]
+k = 3
+mult_indices = [i for i in range(len(input_list)) if (i+1)%k == 0]
+not_mult_indices = [i for i in range(len(input_list)) 
+                    if i not in mult_indices]
+
+def insertion_sort_acs(input_list):
+    for i in range(len(mult_indices)):
+        current = input_list[mult_indices[i]]
+        j = i - 1
+        while j >= 0 and current < input_list[mult_indices[j]]:
+            input_list[mult_indices[j+1]] = input_list[mult_indices[j]]
+            j -= 1
+        input_list[mult_indices[j+1]] = current
+
+def insertion_sort_desc(input_list):
+    for i in range(len(not_mult_indices)):
+        current = input_list[not_mult_indices[i]]
+        j = i - 1
+        while j >= 0 and current > input_list[not_mult_indices[j]]:
+            input_list[not_mult_indices[j+1]] = input_list[not_mult_indices[j]]
+            j -= 1
+        input_list[not_mult_indices[j+1]] = current
+
+
+insertion_sort_acs(input_list)
+insertion_sort_desc(input_list)
+print(input_list)
