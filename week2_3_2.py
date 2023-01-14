adf218c390b2de9aebec77ddc457167d3710d495
SweepingBishops <roshan.thakidiyel@gmail.com>
2023-01-14 07:25:49 +0530

    Corrected qn3 again. Also now checking the multiple for the position and not the index.

--- week2_3_2.py
+++ week2_3_2.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
-def insertion_sort_asc(input_list, check_mult):
+def insertion_sort_desc(input_list, check_mult):
     for i in range(len(input_list)):
-        if i % check_mult == 0:
+        if (i+1) % check_mult == 0:
             continue
         current = input_list[i]
-        if (i-1)%check_mult == 0:
+        if i%check_mult == 0:
             j = i - 2
         else:
             j = i - 1
-        while j >= 0 and input_list[j] > current:
-            if (j+1) % check_mult != 0:
+        while j >= 0 and input_list[j] < current:
+            if (j+2) % check_mult != 0:
                 input_list[j+1] = input_list[j]
             else:
                 input_list[j+2] = input_list[j]
 
-            if (j-1)%check_mult == 0:
+            if (j)%check_mult == 0:
                 j -= 2
             else:
                 j -= 1
 
-        if (j+1) % check_mult != 0:
+        if (j+2) % check_mult != 0:
             input_list[j+1] = current
         else:
             input_list[j+2] = current
@@ -27,11 +27,11 @@ def insertion_sort_asc(input_list, check_mult):
     return input_list
 
 
-def insertion_sort_desc(input_list, check_mult):
-    for i in range(0, len(input_list), check_mult):
+def insertion_sort_asc(input_list, check_mult):
+    for i in range(check_mult-1, len(input_list), check_mult):
         current = input_list[i]
         j = i - check_mult
-        while j >= 0 and input_list[j] < current:
+        while j >= 0 and input_list[j] > current:
             input_list[j + check_mult] = input_list[j]
             j -= check_mult
         input_list[j + check_mult] = current
