3ef24bebf13c7b5db5525501d1ded2bad2325322
SweepingBishops <roshan.thakidiyel@gmail.com>
2023-01-16 15:21:04 +0530

    Edited week3_3 to use dictionaries.

--- week3_3.py
+++ week3_3.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
-def find_index(expression, str_):
-    for index in range(len(expression)):
-        if expression[index] == str_:
-            return index
-    else:
-        return -1
+def create_dict(ORDER):
+    order_dict = dict()
+    for index in range(len(ORDER)):
+        order_dict[ORDER[index]] = index
+    return order_dict
+        
 
-def insertion_sort(input_list, order):
+def insertion_sort(input_list, order_dict):
     for i in range(1, len(input_list)):
         current = input_list[i]
         j = i-1
-        while j >= 0 and find_index(order, input_list[j]) > find_index(order, current):
+        while j >= 0 and order_dict[input_list[j]] > order_dict[current]:
             input_list[j+1] = input_list[j]
             j = j-1
         input_list[j+1] = current
     return input_list
 
+
 INPUT = list("dcabesfshdsakcdc")
 ORDER = list("bacdefshk")
-print(insertion_sort(INPUT, ORDER))
+print(insertion_sort(INPUT, create_dict(ORDER)))
