a5f2d6343363fabded68a080232ff4fb139e2251
SweepingBishops <roshan.thakidiyel@gmail.com>
2023-01-23 16:42:23 +0530

    Week4 qn 1 and 2.

---
+++ week4_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+def evaluate(expression):
+    if len(expression) == 1:
+        return expression[0]
+    level = 0
+    for index in range(len(expression)):
+        char = expression[index]
+        if char == "(":
+            level += 1
+        elif char == ")":
+            level -= 1
+        elif char in "+-":
+            # If level is 1 it means that the operator is one bracket deep.
+            if level == 1:
+                left_expression = expression[1:index]
+                right_expression = expression[index+1:-1]
+                if char == "+":
+                    return int(evaluate(left_expression)) + int(evaluate(right_expression))
+                else:
+                    return int(evaluate(left_expression)) - int(evaluate(right_expression))
+#expression = list("((3+2)-((1-2)+5))")
+expression = list("(1+(1+(1+(1-(1+1)))))")
+print(evaluate(expression))
---
+++ week4_2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+count = 0
+def merge_sort(input_list):
+    if len(input_list) == 1:
+        return input_list
+    mid = len(input_list)//2
+    left_list = merge_sort(input_list[:mid])
+    right_list = merge_sort(input_list[mid:])
+    return merge(left_list, right_list)
+
+def merge(left_list, right_list):
+    global count
+    merged_list = list()
+    while len(left_list) != 0 and len(right_list) != 0:
+        if left_list[0] < right_list[0]:
+            merged_list.append(left_list.pop(0))
+        else:
+            count += 1
+            merged_list.append(right_list.pop(0))
+
+    while len(left_list) != 0:  # Not empty
+        count += 1
+        merged_list.append(left_list.pop(0))
+
+    while len(right_list) != 0:  # Not empty
+        merged_list.append(right_list.pop(0))
+
+    return merged_list
+
+
+if __name__ == "__main__":
+    #sorted_list = merge_sort([68,34,52,12,9,32,67,99])
+    sorted_list = merge_sort([2, 4, 3, 5, 1])
+    assert sorted_list == sorted([2, 4, 3, 5, 1])
+    print(sorted_list)
+    print(count)
