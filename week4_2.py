3d3254b656e766c8c9a9ccca03bb631da2b1dda0
SweepingBishops <roshan.thakidiyel@gmail.com>
2023-01-26 12:26:54 +0530

    Fixed mistakes. Thanks to Methaphur for pointing it out.

--- week4_2.py
+++ week4_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-count = 0
+import week2_4
+from random import shuffle
 
+count = 0
 
 def merge_sort(input_list):
     if len(input_list) == 1:
@@ -15,14 +17,12 @@ def merge(left_list, right_list):
     global count
     merged_list = list()
     while len(left_list) != 0 and len(right_list) != 0:
-        if left_list[0] < right_list[0]:
+        if left_list[0] <= right_list[0]:
             merged_list.append(left_list.pop(0))
         else:
-            count += 1
+            count += len(left_list)
             merged_list.append(right_list.pop(0))
-
     while len(left_list) != 0:  # Not empty
-        count += 1
         merged_list.append(left_list.pop(0))
 
     while len(right_list) != 0:  # Not empty
@@ -32,8 +32,17 @@ def merge(left_list, right_list):
 
 
 if __name__ == "__main__":
-    # sorted_list = merge_sort([68,34,52,12,9,32,67,99])
-    sorted_list = merge_sort([2, 4, 3, 5, 1])
-    assert sorted_list == sorted([2, 4, 3, 5, 1])
-    print(sorted_list)
-    print(count)
+    # Code to check if it actually works.
+    # Uses a previous weeks insertion sort 
+    # alg to check.
+    starter_list = [i for i in range(100)]
+    for i in range(10**3):
+        count = 0
+        print(i)
+        shuffle(starter_list)
+        test_list = starter_list.copy()
+        array = starter_list.copy()
+        correct_count = week2_4.insertion_sort(test_list)
+        merge_sort(starter_list)
+        if count != correct_count:
+            print(f"list: {array}\ncorrect count: {correct_count}\ncount: {count}")
