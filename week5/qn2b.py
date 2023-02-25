c03dafa81c52ef539313a5a16f55099fe4125653
SweepingBishops <roshan.thakidiyel@gmail.com>
2023-02-25 21:39:32 +0530

    Minor changes.

--- week5/qn2b.py
+++ week5/qn2b.py
@@ -4,8 +4,8 @@ def find_max(n, current_row, queens):
     '''
     The function that generates all potential max configurations. And returns max.
     '''
-    # For n <= 4 the answer is 1
-    if n <= 4:
+    # For n <= 3 the answer is 1
+    if n <= 3:
         return 1
     if current_row == n-1:
         return len(queens)
@@ -40,22 +40,19 @@ def check_square(queens, square):
     Takes the values of the queens already on the board and returns
     whether a queen can be kept on a given square or not.
     '''
-    try:
-        if not queens:
-            return True
-        for queen in queens:
-            if square[0] == queen[0] or square[1] == queen[1]:
-                return False
-            elif abs(queen[0] - square[0]) == abs(queen[1] - square[1]):
-                return False
-            elif abs(abs(queen[0] - square[0]) - abs(queen[1] - square[1])) == 1:
-                return False
+    if not queens:
         return True
-    except TypeError:
-        print(queen, square)
-        exit()
+    for queen in queens:
+        if square[0] == queen[0] or square[1] == queen[1]:
+            return False
+        elif abs(queen[0] - square[0]) == abs(queen[1] - square[1]):
+            return False
+        elif abs(abs(queen[0] - square[0]) - abs(queen[1] - square[1])) == 1:
+            return False
+    return True
 
 
 if __name__ == "__main__":
-    n=5
+    import sys
+    n=int(sys.argv[1])
     print(find_max(n,0,[]))
