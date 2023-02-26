e5977d017655ce83ff075c4ffd0d8a31a1832756
SweepingBishops <roshan.thakidiyel@gmail.com>
2023-02-26 10:14:49 +0530

    Tidied up week5 qn2b

--- week5/qn2b.py
+++ week5/qn2b.py
@@ -1,67 +1,63 @@
 #!/usr/bin/env python
-curr_max = 0
-board = []
+
 
 def find_max(n):
-    '''
+    """
     The function that generates all potential max configurations. And returns max.
-    '''
+    """
     # For n <= 3 the answer is 1
     if n <= 3:
-        global curr_max
         curr_max = 1
-        return
-    
+        board = [(0, 0)]
+        return board, curr_max
+    curr_max = 0
+    board = []
 
     def _find_max(current_row, queens):
-        global curr_max
+        nonlocal curr_max
+        nonlocal board
+
         if curr_max == n:
             return
         if current_row == n:
             if len(queens) > curr_max:
                 curr_max = len(queens)
-                global board
                 board = queens
-                return
-        
+            return
+
         new_configs = place_queens(n, current_row, queens)
         for config in new_configs:
-            if len(config)+(n-current_row-1) > curr_max:
-                _find_max(current_row+1, config)
-            # num = _find_max(current_row+1, config)
-            # if num > curr_max:
-            #     curr_max = num
-            #     board = config
+            if len(config) + (n - current_row - 1) > curr_max:
+                _find_max(current_row + 1, config)
 
-    _find_max(0,[])
-        
+    _find_max(0, [])
+    return board, curr_max
 
 
 def place_queens(n, current_row, current_queens):
-    '''
+    """
     Takes the values of the queens that are already on the board
     and tries to keep a queen on every column of the given row.
-    '''
+    """
     if current_row >= n:
         return current_queens
 
     positions = []
     for column in range(n):
         if check_square(current_queens, (current_row, column)):
-            #positions.append([current_queens + [(current_row, column)]])
             positions.append(current_queens + [(current_row, column)])
+
     if positions:
         return positions
     else:
         return [current_queens]
-        
 
 
 def check_square(queens, square):
-    '''
+    """
     Takes the values of the queens already on the board and returns
     whether a queen can be kept on a given square or not.
-    '''
+    """
     if not queens:
         return True
     for queen in queens:
@@ -73,20 +69,23 @@ def check_square(queens, square):
             return False
     return True
 
-def table(config, n):
-    '''Just for pretty printing the boards.'''
-    print("")
+
+def pretty_print(config, n):
+    """Just for pretty printing the boards."""
     for row in range(n):
         for column in range(n):
             if (row, column) in config:
-                print(f"|{chr(9819)}", end = "")
+                print(f"|{chr(9819)}", end="")
             else:
-                print("| ", end = "")
+                print("| ", end="")
         print("|")
+    print("")
+
 
 if __name__ == "__main__":
     import sys
-    n=int(sys.argv[1])
-    find_max(n)
-    table(board, n)
-    print(curr_max)
+
+    n = int(sys.argv[1])
+    board, num = find_max(n)
+    pretty_print(board, n)
+    print(num)
